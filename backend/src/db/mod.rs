02d84d87ddb91b65a499bb502f73827aafc0e614
Tristan Muggridge <muggridge.dev@gmail.com>
2025-05-29 23:07:52 +0900

    implement basic db initialiser for rust backend

---
+++ backend/src/db/mod.rs
@@ -0,0 +1,16 @@
+use rusqlite::{Connection, Result};
+use std::fs;
+
+pub fn initialise() -> Result<()> {
+    let connection = Connection::open("shoyu.db")?;
+
+    let sql_init_file_path = "./migrations/init.sql";
+
+    let sql_file_content =
+        fs::read_to_string(sql_init_file_path).expect("Should have been able to read file");
+
+    println!("{}", &sql_file_content);
+    connection.execute_batch(&sql_file_content)?;
+
+    Ok(())
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,15 @@
+mod db;
+
 use axum::{routing::get, Router};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
+    db::initialise()?; // Call synchronously here and handle error properly
+
     let app = Router::new().route("/", get(|| async { "Hello World!" }));
-
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
     axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
