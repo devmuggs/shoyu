dc46bb3519c767409f70d5fdbe3ddd26f768701a
Tristan Muggridge <muggridge.dev@gmail.com>
2025-06-01 18:08:41 +0900

    adding frontend vite/react starter

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="w-screen h-screen flex items-center justify-center bg-neutral-800 text-white">
+			test content
+		</div>
+	);
+}
+
+export default App;
