18e67da11091cb4ed0ae2329aa55c4f4bcbc5b2d
Tristan Muggridge <muggridge.dev@gmail.com>
2025-06-01 23:22:19 +0900

    fixing styling

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -3,16 +3,16 @@ import { Card, CardContent } from "@/components/ui/card";
 
 export default function SignUp() {
 	return (
-		<div className="flex flex-col items-center justify-center h-full">
-			<h1 className="text-3xl font-semibold mb-6 text-gray-900 dark:text-gray-100">
-				Initial Setup
-			</h1>
-			<div className="grow max-w-md">
-				<Card>
-					<CardContent>
-						<SetupForm />
-					</CardContent>
-				</Card>
+		<div className="h-full flex flex-col items-center justify-center gap-4">
+			<div className="flex flex-col gap-4">
+				<h1 className="text-3xl font-semibold text-gray-900 dark:text-gray-100">Sign Up</h1>
+				<div className="grow max-w-md">
+					<Card>
+						<CardContent>
+							<SetupForm />
+						</CardContent>
+					</Card>
+				</div>
 			</div>
 		</div>
 	);
